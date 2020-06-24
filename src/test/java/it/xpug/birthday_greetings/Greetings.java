2e1f888c8523589f6aeb093037a41f42f79c8ade
serginho6x <serginho_6x@hotmail.com>
2020-06-24 15:10:43 -0400

    Greetings class finished

--- src/test/java/it/xpug/birthday_greetings/Greetings.java
+++ src/test/java/it/xpug/birthday_greetings/Greetings.java
@@ -1,5 +1,39 @@
 package it.xpug.birthday_greetings;
 
 public class Greetings {
-
+	private String subject;
+	private String message;
+	private Employee employee;
+	public Greetings(Employee employee) {
+		this.employee = employee;
+	}
+	
+	public Greetings birthday() {
+		this.setSubject("Happy Birthday!");
+		this.setMessage("Happy Birthday, dear %NAME%!".replace("%NAME%", employee.getFirstName()));
+		return this;
+	}
+	
+	public String getMail() {
+		return employee.getEmail();
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	private void setMessage(String message) {
+		this.message = message;
+		
+	}
+	
+	public String getSubejct() {
+		return subject;
+	}
+	
+	
+	private void setSubject(String subject) {
+		this.subject = subject;
+		
+	}
 }
--- src/test/java/it/xpug/birthday_greetings/IMessageService.java
+++ src/test/java/it/xpug/birthday_greetings/IMessageService.java
@@ -1,5 +1,5 @@
 package it.xpug.birthday_greetings;
 
 public interface IMessageService {
-
+	void send(Greetings greeting );
 }
